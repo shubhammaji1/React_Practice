1425109d9aba730d78ba4e5675da5c0150bfd433
Shubham Maji <122548421+shubhammaji1@users.noreply.github.com>
2024-08-31 16:29:07 +0000

    Components

---
+++ react_basic/src/App.jsx
@@ -0,0 +1,30 @@
+
+// Components 
+import React from "react"
+
+export const App =()=>{
+  return(
+    <div>
+      <Netflix />
+      <Netflix />
+
+    </div>
+  )
+  
+}
+
+
+
+const Netflix=()=>{
+  return (
+    <div>
+      <h2>Movie: V </h2>
+      <h3>Rating: 9.2</h3>
+      <p>Lorem, ipsum dolor sit amet consectetur 
+        adipisicing elit. Modi, nulla! Illum nobis ullam officia consectetur velit sit corrupti
+         dolores, iste, libero inventore unde blanditiis suscipit. Voluptas, quam odit! Nisi, velit.</p>
+    </div>
+  )
+}
+
+
