3b6c8bb5f47a7d8a09540630f07829e88e5215b0
Shubham Maji <122548421+shubhammaji1@users.noreply.github.com>
2024-09-19 19:19:09 +0000

    hooks

--- event_handling/src/components/hooks/States.jsx
+++ event_handling/src/components/hooks/States.jsx
@@ -3,17 +3,26 @@ import React, { useState } from 'react';
 export const States = () => {
     const [count,setCount] = useState(0);
     const handleButtonClick=()=>{
-       setCount(()=>count-1)
+       setCount(()=>count+1)
     };
     return (
         <>
         <section className='main-div'>
         <h1>{count}</h1>
         <button onClick={handleButtonClick}>Click me</button>
-        
         </section>
+        <ChildCom count={count}/>
         </>
+
         
     );
+};
+
+function ChildCom({count}){
+    console.log("Child Component Render");
+    return <div>
+        <h2>Child Component-{count}</h2>
+    </div>
+    
 }
 
