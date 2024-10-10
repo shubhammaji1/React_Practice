ac7bf77d09d95d22ee0c0fdc28297e83245fa7a4
Shubham Maji <122548421+shubhammaji1@users.noreply.github.com>
2024-10-10 18:40:03 +0000

    ReactRouterDom

--- react_router/src/App.jsx
+++ react_router/src/App.jsx
@@ -1,29 +1,53 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider  } from "react-router-dom";
 import { Movie } from "./pages/movie";
 import {Home} from "./pages/Home";
 import { About } from "./pages/about";
 import { Contact } from "./pages/contact";
+import AppLayout from "./components/layout/AppLayout";
 
 const App = ()=>{
 
   const router = createBrowserRouter([
     {
-      path:"/",
-      element:<Home/>
+      path: "/",
+      element:<AppLayout/>,
+      children:[
+        {
+          path:"/",
+          element:<Home/>
+        },
+        {
+          path:"/about",
+          element:<About/>
+        },
+        {
+          path:"/movie",
+          element:<Movie/>
+        },
+        {
+          path:"/contact",
+          element:<Contact/>
+        },
+
+      ]
     },
-    {
-      path:"/about",
-      element:<About/>
-    },
-    {
-      path:"/movie",
-      element:<Movie/>
-    },
-    {
-      path:"/contact",
-      element:<Contact/>
-    },
+   
   ]);
+
+
+  //OLD method
+
+  // const router = createBrowserRouter(
+  //   createRoutesFromElements(
+  //     <Route>
+  //       <Route path="/" element={<Home/>}/>
+  //       <Route path="/about" element={<About/>}/>
+  //       <Route path="/movie" element={<Movie/>}/>
+  //       <Route path="/contact" element={<Contact/>}/>
+  //     </Route>
+  //   )
+
+  // );
 
   return <RouterProvider router={router}/>
 
